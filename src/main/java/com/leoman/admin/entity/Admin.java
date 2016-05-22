3be78d14d788d17f42e29d2890d8f85b1147ead7
ys305751572 <305751572@qq.com>
2016-05-22 13:01:53 +0800

    1

--- src/main/java/com/leoman/admin/entity/Admin.java
+++ src/main/java/com/leoman/admin/entity/Admin.java
@@ -10,50 +10,17 @@ import java.util.Date;
  * Created by Administrator on 2016/3/8.
  */
 @Entity
-@Table(name = "user")
-public class Admin{
-    @Id
-    @GenericGenerator(name = "idGenerator", strategy = "uuid")
-    @GeneratedValue(generator = "idGenerator")
-    @Column(name = "user_id")
-    private String userId;
+@Table(name = "t_admin")
+public class Admin extends BaseEntity{
 
-    @Column(name = "bak4")
-    private String userType;
-
-    @Column(name = "bak5")
-    @Temporal(TemporalType.DATE)
-    private Date createDate;
-
-    public String getUserType() {
-        return userType;
-    }
-
-    public void setUserType(String userType) {
-        this.userType = userType;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public Date getCreateDate() {
-        return createDate;
-    }
-
-    public void setCreateDate(Date createDate) {
-        this.createDate = createDate;
-    }
-
-    @Column(name = "account")
+    @Column(name = "username")
     private String username;
 
     @Column(name = "password")
     private String password;
+
+    @Column(name = "last_login_date")
+    private Long lastLoginDate;
 
     public String getUsername() {
         return username;
@@ -69,5 +36,13 @@ public class Admin{
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public Long getLastLoginDate() {
+        return lastLoginDate;
+    }
+
+    public void setLastLoginDate(Long lastLoginDate) {
+        this.lastLoginDate = lastLoginDate;
     }
 }
