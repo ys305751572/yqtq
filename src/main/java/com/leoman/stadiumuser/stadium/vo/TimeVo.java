0c30aa8ee1554e33f62ecc86b0f991e6593a0f52
touxiangshicaiquan <453550046@qq.com>
2016-08-17 16:38:20 +0800

    提交代码

---
+++ src/main/java/com/leoman/stadiumuser/stadium/vo/TimeVo.java
@@ -0,0 +1,39 @@
+package com.leoman.stadiumuser.stadium.vo;
+
+/**
+ *
+ * Created by Administrator on 2016/8/17.
+ */
+
+public class TimeVo {
+
+    private Integer year;
+
+    private Integer month;
+
+    private Integer day;
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public Integer getMonth() {
+        return month;
+    }
+
+    public void setMonth(Integer month) {
+        this.month = month;
+    }
+
+    public Integer getDay() {
+        return day;
+    }
+
+    public void setDay(Integer day) {
+        this.day = day;
+    }
+}
